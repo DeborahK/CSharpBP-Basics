63975a22e570020c518d6a41ad7650e0fdd3f514
DeborahK <deborahk@insteptech.com>
2015-08-27 16:42:43 -0700

    Added a second service.

---
+++ AcmeApp/Acme.Common/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Common
+{
+    public class EmailService
+    {
+        public string SayHello()
+        {
+            return "Hello from the email service";
+        }
+    }
+}
---
+++ AcmeApp/Tests/Acme.CommonTests/EmailServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Common.Tests
+{
+    [TestClass()]
+    public class EmailServiceTests
+    {
+        [TestMethod()]
+        public void SayHello_Success()
+        {
+            // Arrange
+            var expected = "Hello from the email service";
+            var email = new EmailService();
+
+            // Act
+            var actual = email.SayHello();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
