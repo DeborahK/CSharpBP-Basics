1d782b0f0f288fbc806dc56124d0de3e4069a977
DeborahK <deborahk@insteptech.com>
2015-08-28 12:48:23 -0700

    Added a second method to the LoggingService.

--- AcmeApp/Acme.Common/LoggingService.cs
+++ AcmeApp/Acme.Common/LoggingService.cs
@@ -8,6 +8,11 @@ namespace Acme.Common
 {
     public class LoggingService
     {
+        public void LogAction(string action)
+        {
+            Console.WriteLine(DateTime.Now + ": " + action);
+        }
+
         public string SayHello()
         {
             return "Hello from the logging service";
