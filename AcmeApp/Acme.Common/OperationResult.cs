43e03aff4a8235b8825edbf0e47c94f8f5e7fa89
DeborahK <deborahk@insteptech.com>
2015-09-08 10:30:06 -0700

    Added OperationResult

---
+++ AcmeApp/Acme.Common/OperationResult.cs
@@ -0,0 +1,18 @@
+﻿namespace Acme.Common
+{
+    public class OperationResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        public OperationResult()
+        {
+        }
+
+        public OperationResult(bool success, string message) : this()
+        {
+            this.Success = success;
+            this.Message = message;
+        }
+    }
+}
